3c25d7c426ed9854f8e7d7431f415e54ebc157f6
angeljmercado <mercadomilletangel@gmail.com>
2023-09-26 09:31:07 -0700

    create class that generates the request in the backend

---
+++ backend.py
@@ -0,0 +1,17 @@
+import openai
+import sys
+from decouple import config
+
+api_key = config("key")
+class ChatBot:
+    def __init__(self):
+        openai.api_key = api_key
+    
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=4000,
+            temperature=0.5
+            ).choices[0].text
+        return response
